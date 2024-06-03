f31a49bb6a2dc3052d648a4af1c35edbb5648488
Junior11995 <junior22ascinsero@gmail.com>
2024-06-03 01:01:57 -0400

    Agregar archivos de proyecto.

---
+++ CadenaFucionStrlwrPalabraMinuscula/CadenaFucionStrlwrPalabraMinuscula.cpp
@@ -0,0 +1,25 @@
+/*
+* Bloque 7 cadenas de caracteres
+* 
+* Ejercicio 7
+* 
+* Pedir su nombre al usuario en MAYUSCULA 
+* Si su nombre comienza por la letra A
+* convertir en minuscula 
+* en caso comtrario no convertirlo
+*/
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+
+int main() {
+
+
+
+
+	_getch();
+	return 0;
+}
